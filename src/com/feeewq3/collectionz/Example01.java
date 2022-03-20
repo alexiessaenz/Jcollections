885c012bfd464163d298197c5b15a9671c4fb9b3
feeewq3 <00400815@uca.edu.sv>
2022-03-19 21:56:53 -0600

    first commit

---
+++ src/com/feeewq3/collectionz/Example01.java
@@ -0,0 +1,30 @@
+package com.feeewq3.collectionz;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(8);
+		numbers.add(11);
+		numbers.add(34);
+		numbers.add(5);
+		numbers.add(7);
+		
+		for (int i = 0; i < numbers.size(); i++) {
+			int value = numbers.get(i);
+			System.out.println("Item "+ i + ": "+ value);
+		}
+		
+		Iterator<Integer> numbersIterator = numbers.iterator();
+		while (numbersIterator.hasNext()) {
+			int value = (int) numbersIterator.next();
+			System.out.println("Item " + ": "+ value);
+		} 
+
+	}
+
+}
